4471fa9f3cdd707f0860406c7a48209e0165a0e5
deevlas <deevlas@icloud.com>
2020-07-07 18:19:35 +0300

    add five array

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// 1. -- створити об'єкт (не меньше 5ти властивостей) який описує
+// - собаку
+// - людину
+// - автомобіль
+// - квартиру
+// - книгу
+
+
 let dog = {
     name: "Sharik",
     age: 3,
@@ -37,3 +45,19 @@ let book = {
     pages: 480,
     publicationData: 1840
 }
+
+
+// 2. -- Створити масив та вивести його в консоль:
+// - з 5 собак
+// - 3 5 людей
+// - з 5 автомобілів
+
+
+let arrayWithDogs = ["Такса", "Французский бульдог", "Хаски", "Лабрадор", "Колли"];
+console.log(arrayWithDogs)
+
+let arrayWithPeople = ["Oleg", "Viktor", "Nadya", "Mariia", "Vova"];
+console.log(arrayWithPeople);
+
+let arrayWithCars = ["Volvo", "Kia", "Toyota", "Honda", "Mazda"]
+console.log(arrayWithCars)
